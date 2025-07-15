f53fde0b5085e133928b8a23b69411c0f8e07066
islam larabi <islamlarabi0@gmail.com>
2025-07-15 19:31:12 +0100

    Add user creation validation to prevent duplicate email registration

--- block/main.py
+++ block/main.py
@@ -84,23 +84,33 @@ def update_item(item_id: int, price: int, db: Session = Depends(get_db)):
 
 @app.post("/users", status_code=status.HTTP_201_CREATED,response_model=scemat.UserResponse)
 def creat_user(user: scemat.User, db: Session = Depends(get_db)):
+    # Vérifier si l'utilisateur existe déjà
+    existing_user = db.query(modal.user).filter(modal.user.email == user.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
+
+
     hashed_password = Hash.hash_password(user.hashed_password)
+
+
     new_user = modal.user(
         name=user.name,
         email=user.email,
         hashed_password=hashed_password,
         is_active=user.is_active
     )
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    db.close()
 
     return new_user
 
 @app.get("/users", status_code=status.HTTP_200_OK, response_model=list[scemat.UserResponse])
 def get_users(db: Session = Depends(get_db)):
     users = db.query(modal.user).all()
-    db.close()
     return users
 
