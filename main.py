a4a2536dd7e75123f95c0a2653fd9f5fab9b32a6
islam larabi <islamlarabi0@gmail.com>
2025-07-17 12:45:17 +0100

    Remove main.py and add requirements.txt for project dependencies

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
+from block import scemat
+from block.database import engine, SessionLocal
+from block import modal
+from block.routers import user, items
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
+
+# This will create all tables defined with Base (including user)
+modal.Base.metadata.create_all(bind=engine)
+
 
 
 app = FastAPI()
 
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items")
-def read_item():
-    return {"item_id":' lmlm'}
-
-@app.post("/items/")
-def create_item(item: Item):
-    return {"item_name": item.name, "item_price": item.price, "item_is_offer": item.is_offer}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-
-
+app.include_router(user.router)
+app.include_router(items.router)
 
 
 
