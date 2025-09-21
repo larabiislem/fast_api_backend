0dce06b9387658c82022c6e6caa6ecd95b1aa3f4
islam larabi <islamlarabi0@gmail.com>
2025-09-21 22:17:26 +0100

    Remove comment on table creation in main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from block import modal
 from block.routers import user, items
 from fastapi import FastAPI
 
-# This will create all tables defined with Base (including user)
 modal.Base.metadata.create_all(bind=engine)
 
 
 
 app = FastAPI()
+
+
+
 
 
 app.include_router(user.router)
